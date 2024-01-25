b7ef7983b2d4972f2aec4377e85bb6601dc458f8
shinkee635 <shinkee@hotmail.com>
2024-01-26 06:51:24 +0800

    Update install_boost_1_73.sh

--- scripts/install_boost_1_73.sh
+++ scripts/install_boost_1_73.sh
@@ -3,10 +3,8 @@ set -e
 
 BOOST_VER=1.73.0
 BOOST_VERSION=1_73_0
-echo "LD_LIBRARY_PATH=$GITHUB_WORKSPACE/solidity/build/deps/lib" >> $GITHUB_ENV
-echo "CPATH=$GITHUB_WORKSPACE/solidity/build/deps/include" >> $GITHUB_ENV
-echo "C_INCLUDE_PATH=$GITHUB_WORKSPACE/solidity/build/deps/include" >> $GITHUB_ENV
-echo "CPLUS_INCLUDE_PATH=$GITHUB_WORKSPACE/solidity/build/deps/include" >> $GITHUB_ENV
+echo "BOOST_LIBRARYDIR=$GITHUB_WORKSPACE/solidity/build/deps/lib" >> $GITHUB_ENV
+echo "BOOST_INCLUDEDIR=$GITHUB_WORKSPACE/solidity/build/deps/include" >> $GITHUB_ENV
 DLDIR="$GITHUB_WORKSPACE/solidity/deps/downloads"
 mkdir -p "$DLDIR"
 (
