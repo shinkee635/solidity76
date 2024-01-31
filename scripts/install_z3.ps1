af8d3222d215c07705087ab3eed9b023e7470884
shinkee635 <shinkee@hotmail.com>
2024-02-01 06:57:44 +0800

    Test full

--- scripts/install_z3.ps1
+++ scripts/install_z3.ps1
@@ -10,7 +10,7 @@ Expand-Archive -LiteralPath z3.zip -DestinationPath .\
 cd z3-z3-4.8.9
 mkdir build
 cd build
-cmake -G "Visual Studio 17 2022" -DZ3_BUILD_LIBZ3_SHARED=OFF -DCMAKE_INSTALL_PREFIX="$githubWorkspace\build\deps\z3" -DCMAKE_BUILD_TYPE=Release ..
+cmake -G "Visual Studio 17 2022" -DZ3_BUILD_LIBZ3_SHARED=OFF -DCMAKE_INSTALL_PREFIX="$githubWorkspace\build\deps\z3" ..
 cmake --build . --config Release --target libz3 -j 12
 cmake --install .
 
