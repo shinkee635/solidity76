6c5a6cc8dc4da875113c224be5a9e13a591ec0ea
shinkee635 <shinkee@hotmail.com>
2024-02-01 01:55:14 +0800

    Test installing z3

---
+++ scripts/install_z3.ps1
@@ -0,0 +1,7 @@
+$ErrorActionPreference = "Stop"
+
+# Needed for Invoke-WebRequest to work via CI.
+$progressPreference = "silentlyContinue"
+
+Invoke-WebRequest -URI "https://github.com/Z3Prover/z3/releases/download/z3-4.8.9/z3-4.8.9-x64-win.zip" -OutFile "z3.zip"
+tar -xf z3.zip "deps"
