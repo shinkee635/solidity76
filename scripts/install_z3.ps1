06026311d59ce4d264e90494ee334d3f7d7fe571
shinkee635 <shinkee@hotmail.com>
2024-02-01 05:17:22 +0800

    Debug

--- scripts/install_z3.ps1
+++ scripts/install_z3.ps1
@@ -11,9 +11,13 @@ cd z3-z3-4.8.9
 mkdir build
 cd build
 cmake -G "Visual Studio 17 2022" -DZ3_BUILD_LIBZ3_SHARED=OFF -DCMAKE_INSTALL_PREFIX="$githubWorkspace\build\deps\z3" -DCMAKE_BUILD_TYPE=Release ..
+echo "cmake building"
 cmake --build . --config Release --target libz3 -j
+echo "msbuild z3.sln"
 msbuild z3.sln /p:Configuration=Release /m:5 /v:minimal
+echo "Done building"
 
 # Export to environment variable
 "Z3_DIR=$githubWorkspace\build\deps\z3" | Out-File -Append -FilePath $Env:GITHUB_ENV
+echo "Back to previous directory"
 cd ..\..
