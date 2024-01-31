b25c1aca8cbf837d08a094f8d93c0f698c0036e6
shinkee635 <shinkee@hotmail.com>
2024-02-01 02:14:33 +0800

    Z3 again

--- scripts/install_z3.ps1
+++ scripts/install_z3.ps1
@@ -5,6 +5,6 @@ $progressPreference = "silentlyContinue"
 
 mkdir deps
 Invoke-WebRequest -URI "https://github.com/Z3Prover/z3/releases/download/z3-4.8.9/z3-4.8.9-x64-win.zip" -OutFile "z3.zip"
-Expand-Archive -LiteralPath z3.zip
+Expand-Archive -LiteralPath z3.zip -DestinationPath .\
 mv z3-4.8.9-x64-win\bin deps
 mv z3-4.8.9-x64-win\include deps
