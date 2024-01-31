bcbce30a1506552cf7ac134e347579720b824028
shinkee635 <shinkee@hotmail.com>
2024-02-01 01:57:22 +0800

    Test z3 again

--- scripts/install_z3.ps1
+++ scripts/install_z3.ps1
@@ -4,4 +4,5 @@ $ErrorActionPreference = "Stop"
 $progressPreference = "silentlyContinue"
 
 Invoke-WebRequest -URI "https://github.com/Z3Prover/z3/releases/download/z3-4.8.9/z3-4.8.9-x64-win.zip" -OutFile "z3.zip"
+mkdir deps
 tar -xf z3.zip "deps"
