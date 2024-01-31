387a9a576e3b400949ab7e72420fc2780aeb5c6b
shinkee635 <shinkee@hotmail.com>
2024-02-01 02:53:29 +0800

    Test z3 cmake build

--- scripts/install_z3.ps1
+++ scripts/install_z3.ps1
@@ -1,10 +1,16 @@
 $ErrorActionPreference = "Stop"
 
+$githubWorkspace = $Env:GITHUB_WORKSPACE
 # Needed for Invoke-WebRequest to work via CI.
 $progressPreference = "silentlyContinue"
 
-mkdir deps
-Invoke-WebRequest -URI "https://github.com/Z3Prover/z3/releases/download/z3-4.8.9/z3-4.8.9-x64-win.zip" -OutFile "z3.zip"
+mkdir build
+mkdir build\deps
+Invoke-WebRequest -URI "https://github.com/Z3Prover/z3/archive/refs/tags/z3-4.8.9.zip" -OutFile "z3.zip"
 Expand-Archive -LiteralPath z3.zip -DestinationPath .\
-mv z3-4.8.9-x64-win\bin deps
-mv z3-4.8.9-x64-win\include deps
+cd z3-z3-4.8.9
+mkdir build
+cd build
+cmake -DZ3_BUID_LIBZ3_SHARED=OFF -DCMAKE_INSTALL_PREFIX="$githubWorkspace\build\deps\z3" -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . --config Release --target libz3 -j
+cmake --install
