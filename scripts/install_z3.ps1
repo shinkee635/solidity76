43dcadafbfa270d1094b0ea99c259238716ed115
shinkee635 <shinkee@hotmail.com>
2024-02-01 02:08:59 +0800

    Z3 again

--- scripts/install_z3.ps1
+++ scripts/install_z3.ps1
@@ -3,5 +3,8 @@ $ErrorActionPreference = "Stop"
 # Needed for Invoke-WebRequest to work via CI.
 $progressPreference = "silentlyContinue"
 
+mkdir deps
 Invoke-WebRequest -URI "https://github.com/Z3Prover/z3/releases/download/z3-4.8.9/z3-4.8.9-x64-win.zip" -OutFile "z3.zip"
-Expand-Archive -LiteralPath z3.zip -DestinationPath .\deps
+Expand-Archive -LiteralPath z3.zip
+mv z3-4.8.9-x64-win\bin deps
+mv z3-4.8.9-x64-win\include deps
