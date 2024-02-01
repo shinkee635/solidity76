13e5b42783b80aaff50ff322026177055cdedb1f
shinkee635 <shinkee@hotmail.com>
2024-02-01 16:26:12 +0800

    Test with Boost 1.78

--- scripts/install_deps.ps1
+++ scripts/install_deps.ps1
@@ -3,14 +3,16 @@ $ErrorActionPreference = "Stop"
 # Needed for Invoke-WebRequest to work via CI.
 $progressPreference = "silentlyContinue"
 
-if ( -not (Test-Path "$PSScriptRoot\..\deps\boost") ) {
+if ( -not (Test-Path "$PSScriptRoot\..\build\deps\boost") ) {
   New-Item -ItemType Directory -Force -Path "$PSScriptRoot\..\deps"
 
-  Invoke-WebRequest -URI "https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.zip" -OutFile boost.zip
-  tar -xf boost.zip
-  cd boost_1_76_0
+  Invoke-WebRequest -URI "https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.zip"
+  #Invoke-WebRequest -URI "https://boostorg.jfrog.io/artifactory/main/release/1.84.0/source/boost_1_84_0.zip"
+  #tar -xf boost_1_84_0.zip
+  tar -xf boost_1_78_0.zip
+  cd boost_1_78_0
   .\bootstrap.bat
-  .\b2 -j4 -d0 link=static runtime-link=static variant=release threading=multi address-model=64 --with-filesystem --with-system --with-program_options --with-test --prefix="$PSScriptRoot\..\deps\boost" install
+  .\b2 -j4 -d0 link=static runtime-link=static variant=release threading=multi address-model=64 --with-filesystem --with-system --with-program_options --with-test --prefix="$PSScriptRoot\..\build\deps\boost" install
   if ( -not $? ) { throw "Error building boost." }
   cd ..
 }
