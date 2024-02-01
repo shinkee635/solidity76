192ea2dd1c87bdf91424de0224ea848e59703466
shinkee635 <shinkee@hotmail.com>
2024-02-01 17:02:36 +0800

    Test Boost 1.78

--- scripts/install_deps.ps1
+++ scripts/install_deps.ps1
@@ -4,12 +4,12 @@ $ErrorActionPreference = "Stop"
 $progressPreference = "silentlyContinue"
 
 if ( -not (Test-Path "$PSScriptRoot\..\build\deps\boost") ) {
-  New-Item -ItemType Directory -Force -Path "$PSScriptRoot\..\deps"
+  New-Item -ItemType Directory -Force -Path "$PSScriptRoot\..\build\deps"
 
-  Invoke-WebRequest -URI "https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.zip"
+  Invoke-WebRequest -URI "https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.zip" -OutFile boost_1_78_0.7z
   #Invoke-WebRequest -URI "https://boostorg.jfrog.io/artifactory/main/release/1.84.0/source/boost_1_84_0.zip"
   #tar -xf boost_1_84_0.zip
-  tar -xf boost_1_78_0.zip
+  7z x boost_1_78_0.7z -o . -y
   cd boost_1_78_0
   .\bootstrap.bat
   .\b2 -j4 -d0 link=static runtime-link=static variant=release threading=multi address-model=64 --with-filesystem --with-system --with-program_options --with-test --prefix="$PSScriptRoot\..\build\deps\boost" install
--- scripts/install_z3.ps1
+++ scripts/install_z3.ps1
@@ -4,16 +4,14 @@ $githubWorkspace = $Env:GITHUB_WORKSPACE
 # Needed for Invoke-WebRequest to work via CI.
 $progressPreference = "silentlyContinue"
 
-mkdir build\deps\z3 -Force
+mkdir build\deps -Force
 Invoke-WebRequest -URI "https://github.com/Z3Prover/z3/archive/refs/tags/z3-4.8.9.zip" -OutFile "z3.zip"
 Expand-Archive -LiteralPath z3.zip -DestinationPath .\
 cd z3-z3-4.8.9
 mkdir build
 cd build
-cmake -G "Visual Studio 17 2022" -DZ3_BUILD_LIBZ3_SHARED=OFF -DCMAKE_INSTALL_PREFIX="$githubWorkspace\build\deps\z3" -DCMAKE_BUILD_TYPE=Release ..
+cmake -G "Visual Studio 17 2022" -DZ3_BUILD_LIBZ3_SHARED=OFF -DCMAKE_INSTALL_PREFIX="..\..\build\deps\z3" -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release -j 12
 cmake --install .
 
-# Export to environment variable
-"Z3_DIR=$githubWorkspace\build\deps\z3\lib\cmake\z3" | Out-File -Append -FilePath $Env:GITHUB_ENV
 cd ..\..
