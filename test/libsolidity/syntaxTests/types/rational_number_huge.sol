3f1a2dcf12e4bb4f51e3f15bf22809574b7a0137
shinkee635 <shinkee@hotmail.com>
2024-03-28 05:17:12 +0800

    Update rational_number_huge.sol

--- test/libsolidity/syntaxTests/types/rational_number_huge.sol
+++ test/libsolidity/syntaxTests/types/rational_number_huge.sol
@@ -1,10 +1,10 @@
 contract C {
     function f(uint y) public pure {
         // fits FixedBytes with exactly 32-bytes
-        y = uint(ffffffff00000000ffffffff00000000ffffffff00000000ffffffff00000000); // FixedBytes (32)
+        y = uint(0xffffffff00000000ffffffff00000000ffffffff00000000ffffffff00000000); // FixedBytes (32)
 
         // fits exactly into FixedBytes (32), ensures underscored literals won't hurt
-        y = uint(ffffffff00000000ffffffff00000000ffffffff00000000ffffffff_00000000);
+        y = uint(0xffffffff00000000ffffffff00000000ffffffff00000000ffffffff_00000000);
     }
 }
 // ----
