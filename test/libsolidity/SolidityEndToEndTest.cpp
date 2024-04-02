ff0a36cda9dba348661bc613ff03f7f82086aec2
shinkee635 <shinkee@hotmail.com>
2024-04-03 00:00:40 +0800

    Update SolidityEndToEndTest.cpp

--- test/libsolidity/SolidityEndToEndTest.cpp
+++ test/libsolidity/SolidityEndToEndTest.cpp
@@ -1543,7 +1543,7 @@ BOOST_AUTO_TEST_CASE(event)
 			event Deposit(address indexed _from, bytes32 indexed _id, uint _value);
 			function deposit(bytes32 _id, bool _manually) public payable {
 				if (_manually) {
-					bytes32 s = bytes32(0x19dacbf83c5de6658e14cbf7bcae5c15eca2eedecf1c66fbca928e4d351bea0f);
+					bytes32 s = bytes32(uint256(0x0019dacbf83c5de6658e14cbf7bcae5c15eca2eedecf1c66fbca928e4d351bea0f));
 					log3(bytes32(msg.value), s, bytes32(uint256(msg.sender)), _id);
 				} else {
 					emit Deposit(msg.sender, _id, msg.value);
