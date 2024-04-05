79bf8adaaced6e919be855ab86e95afdd655999d
shinkee635 <shinkee@hotmail.com>
2024-04-05 23:07:01 +0800

    Updated expected output

--- test/libsolidity/smtCheckerTests/types/fixed_bytes_1.sol
+++ test/libsolidity/smtCheckerTests/types/fixed_bytes_1.sol
@@ -13,4 +13,4 @@ contract C
 }
 // ----
 // Warning 6328: (96-110): CHC: Assertion violation happens here.\nCounterexample:\nx = 0\ny = 0\n\n\nTransaction trace:\nconstructor()\nState: x = 0\nf(0)
-// Warning 6328: (114-130): CHC: Assertion violation happens here.\nCounterexample:\nx = 0\ny = 0\n\n\nTransaction trace:\nconstructor()\nState: x = 0\nf(0)
+// Warning 6328: (114-130): CHC: Assertion violation happens here.\nCounterexample:\nx = 0\ny = 1\n\n\nTransaction trace:\nconstructor()\nState: x = 0\nf(1)
