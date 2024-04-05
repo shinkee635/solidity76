c7cee407af8d147b6b6457c57761228315386411
shinkee635 <shinkee@hotmail.com>
2024-04-05 22:49:04 +0800

    Update type_conversion_cleanup.sol

--- test/libsolidity/semanticTests/types/type_conversion_cleanup.sol
+++ test/libsolidity/semanticTests/types/type_conversion_cleanup.sol
@@ -1,5 +1,5 @@
 contract Test {
-    function test() public returns (uint ret) { return uint(address(Test(address(0x11223344556677889900112233445566778899001122334455667788990011223344)))); }
+    function test() public returns (uint ret) { return uint(address(Test(address(0x3344556677889900112233445566778899001122334455667788990011223344)))); }
 }
 // ====
 // compileViaYul: also
