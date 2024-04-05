24c010717a66928e4b8a36b0fde742cb18630745
shinkee635 <shinkee@hotmail.com>
2024-04-05 16:51:23 +0800

    Update Interpreter.h

--- test/tools/yulInterpreter/Interpreter.h
+++ test/tools/yulInterpreter/Interpreter.h
@@ -75,16 +75,16 @@ struct InterpreterState
 	/// This is different than memory.size() because we ignore gas.
 	u256 msize;
 	std::map<util::h256, util::h256> storage;
-	u160 address = 0x11111111;
+	u256 address = 0x11111111;
 	u256 balance = 0x22222222;
 	u256 selfbalance = 0x22223333;
-	u160 origin = 0x33333333;
-	u160 caller = 0x44444444;
+	u256 origin = 0x33333333;
+	u256 caller = 0x44444444;
 	u256 callvalue = 0x55555555;
 	/// Deployed code
 	bytes code = util::asBytes("codecodecodecodecode");
 	u256 gasprice = 0x66666666;
-	u160 coinbase = 0x77777777;
+	u256 coinbase = 0x77777777;
 	u256 timestamp = 0x88888888;
 	u256 blockNumber = 1024;
 	u256 difficulty = 0x9999999;
